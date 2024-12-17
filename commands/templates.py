29c951d4e14862e552debdd3a059abb453262ef8
Myles1451 <myles14511@gmail.com>
2024-12-17 15:15:04 +0000

    Update templates.py

--- commands/templates.py
+++ commands/templates.py
@@ -77,7 +77,7 @@ class TemplatesCommands(commands.Cog):
 
             await ctx.message.edit("💡 Template \"Omori\".", delete_after=config_selfbot.deltime)
         elif choice == "cod":
-            assets = {"large_image": self.templates_assets["cod"]["large_image"],
+            assets = {"cod_large_image": self.templates_assets["cod"]["cod_large_image"],
                       "large_text": "Call Of Duty: MWIII",
                       "small_image": self.templates_assets["cod"]["small_image"],
                       "small_text": "Battle Pass level 21"
